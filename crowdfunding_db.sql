c1314cddad0a46400cdc87cf0d4fb8419be30788
Jared Sletto <135926755+jaredsletto@users.noreply.github.com>
2023-09-04 21:50:47 -0600

    Add files via upload

--- crowdfunding_db.sql
+++ crowdfunding_db.sql
@@ -5,54 +5,56 @@ drop table if exists category;
 drop table if exists subcategory;
 drop table if exists campaign;
 
--- create contacts table and set our primary key
-
-CREATE TABLE contacts (
-    contact_id VARCHAR(4) NOT NULL,
-    first_name VARCHAR (150)  NOT NULL,
-    last_name VARCHAR (150)  NOT NULL,
-    email VARCHAR (150)  NOT NULL,
-	primary key (contact_id)
-);
-
--- create category table and set our primary key
-
-CREATE TABLE category (
-    category_id varchar (150)   NOT NULL,
-    category varchar (150)  NOT NULL,
-	primary key (category_id)
-);
-
--- create subcategory table and set our primary key
-
-CREATE TABLE subcategory (
-    subcategory_id varchar (150)  NOT NULL,
-    subcategory varchar (150)  NOT NULL,
-	primary key (subcategory_id)
-);
-
 -- create campaign table and set our primary key and foreign keys
 
-CREATE TABLE campaign (
-    cf_id VARCHAR (5) NOT NULL,
-    contact_id VARCHAR (4) NOT NULL,
-    company_name VARCHAR (50)  NOT NULL,
-    description VARCHAR (100)  NOT NULL,
-    goal DECIMAL NOT NULL,
-    pledged DECIMAL   NOT NULL,
-    outcome VARCHAR (20)  NOT NULL,
-    backers_count INT NOT NULL,
-    country VARCHAR (2)  NOT NULL,
-    currency VARCHAR (3)  NOT NULL,
-    launched_date DATE NOT NULL,
-    end_date DATE NOT NULL,
-    category_id VARCHAR (5)  NOT NULL,
-    subcategory_id VARCHAR (5)  NOT NULL,
-	primary key (cf_id),
+CREATE TABLE "campaign" (
+    "cf_id" INT   NOT NULL,
+    "contact_id" INT   NOT NULL,
+    "company_name" VARCHAR   NOT NULL,
+    "description" VARCHAR   NOT NULL,
+    "goal" DECIMAL   NOT NULL,
+    "pledged" DECIMAL   NOT NULL,
+    "outcome" VARCHAR   NOT NULL,
+    "barckers_count" INT   NOT NULL,
+    "country" VARCHAR   NOT NULL,
+    "currency" VARCHAR   NOT NULL,
+    "launched_date" DATE   NOT NULL,
+    "end_date" DATE   NOT NULL,
+    "category_id" VARCHAR   NOT NULL,
+    "subcategory_id" VARCHAR   NOT NULL,
+    primary key (cf_id),
 	foreign key (contact_id) references contacts(contact_id),
 	foreign key (category_id) references category(category_id),
 	foreign key (subcategory_id) references subcategory(subcategory_id)
 );
+
+-- create category table and set our primary key
+
+CREATE TABLE "category" (
+    "category_id" VARCHAR   NOT NULL,
+    "category" VARCHAR   NOT NULL,
+    primary key ("category_id")
+);
+
+-- create contacts table and set our primary key
+
+CREATE TABLE "contacts" (
+    "contact_id" INT   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "email" VARCHAR   NOT NULL,
+    primary key ("contact_id")
+);
+
+-- create subcategory table and set our primary key
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" VARCHAR   NOT NULL,
+    "subcategory" VARCHAR   NOT NULL,
+    primary key ("subcategory_id")
+);
+
+-- Create select commands
 
 select * from contacts
 select * from category
